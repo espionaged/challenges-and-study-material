1f88430a335a63db8f8f68084d8de4f582fbc2f9
Kevin Gonzalez <kgonzale@cbu.edu>
2018-10-27 23:42:02 -0500

    first medium done

--- medium/scramblies.js
+++ medium/scramblies.js
@@ -1,6 +1,15 @@
 // https://www.codewars.com/kata/scramblies/train/javascript
-
 function scramble(str1, str2) {
   const sortStr1 = str1.split("").sort();
   const sortStr2 = str2.split("").sort();
+
+  let i = 0;
+
+  for (let j = 0; j < sortStr1.length && i < sortStr2.length; j++) {
+    if (sortStr1[j] === sortStr2[i]) {
+      i++;
+    }
+  }
+
+  return i === sortStr2.length;
 }
